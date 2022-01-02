94ca170f09bc6c2283f8376ad262d40cc161592c
Mbah Neilvani <neilvanimbah@gmail.com>
2022-01-02 22:54:27 +0100

    even or odd

---
+++ Js/Even or Odd.js
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOdd(a){
+    if(a % 2){
+        return Even;
+    }else{
+        return Odd;
+    }
+}
+
